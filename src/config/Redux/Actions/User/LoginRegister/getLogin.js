1637d178c819c641acac64ab188202c9b3eaa697
guswawan <agsn.does6@gmail.com>
2020-01-18 11:15:29 +0700

    commit user

--- src/config/Redux/Actions/User/LoginRegister/getLogin.js
+++ src/config/Redux/Actions/User/LoginRegister/getLogin.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
-const PORT = process.env.PORT
-const HOST = process.env.HOST
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
 
-const URL_STRING = `http://192.168.6.169:5000/user/login`;
+const URL_STRING = `http://${HOST}:8000/user/login`;
 
 export const getLogin = (email, password, role) => {
   return {
     type: 'GET_LOGIN',
-    payload: axios.get(URL_STRING,{
+    payload: axios.get(URL_STRING, {
       params: {
         email: email,
         password: password,
-        role: role
-      }
-    })
+        role: role,
+      },
+    }),
   };
 };
--- src/config/Redux/Actions/User/LoginRegister/postRegister.js
+++ src/config/Redux/Actions/User/LoginRegister/postRegister.js
@@ -1,20 +1,16 @@
 import axios from 'axios';
-const PORT = process.env.PORT
-const HOST = process.env.HOST
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
 
-const URL_STRING = `http://192.168.6.169:5000/user/register`;
+const URL_STRING = `http://${HOST}:8000/user/register`;
 
-export const postRegister = (data) => {
+export const postRegister = data => {
   return {
     type: 'POST_REGISTER',
-    payload: axios.post(
-      URL_STRING,
-      data,
-      {
-        header: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
+    payload: axios.post(URL_STRING, data, {
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded',
       },
-    ),
+    }),
   };
 };
