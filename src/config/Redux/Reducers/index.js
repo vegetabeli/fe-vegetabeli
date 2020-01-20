60a17edf0ffdf4aaf9f37151acb263d2df87e4b4
pujakesuma <ahmadridwan.3107@gmail.com>
2020-01-20 08:15:07 +0700

    update new

--- src/config/Redux/Reducers/index.js
+++ src/config/Redux/Reducers/index.js
@@ -6,6 +6,8 @@ import emailInsert from '../Reducers/User/ForgotPassword/emailInsert'
 import codeInsert from '../Reducers/User/ForgotPassword/codeInsert'
 import passwordInsert from '../Reducers/User/ForgotPassword/passwordInsert'
 import getLoginPhone from '../Reducers/User/LoginRegister/getLoginPhone'
+import patchUserBuyer from '../Reducers/User/CrudUser/patchUserBuyer'
+import getUserBuyer from '../Reducers/User/CrudUser/getUserBuyer'
 
 const reducers = combineReducers({
   postRegister,
@@ -13,7 +15,9 @@ const reducers = combineReducers({
   emailInsert,
   codeInsert,
   passwordInsert,
-  getLoginPhone
+  getLoginPhone,
+  patchUserBuyer,
+  getUserBuyer
 })
 
 export default reducers
